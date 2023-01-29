481c983819dac0ee835ed59065af0a6986fb8868
Facelesscactus <113211038+Facelesscactus@users.noreply.github.com>
2023-01-29 15:50:53 -0700

    Updating DiscoveryGame

--- src/DiscoveryGame/DiscoveryGame.js
+++ src/DiscoveryGame/DiscoveryGame.js
@@ -6,27 +6,6 @@ import scope from '../imgs/ScaryJerma.png'
 import MrElbertson from '../imgs/jerma-burger.gif'
 import Jermy from '../imgs/jerma.gif'
  
-/*
-const styles = {
-    slide: {
-      padding: 15,
-      minHeight: 100,
-      color: '#fff',
-    },
-    slide1: {
-      backgroundColor: '#FEA900',
-    },
-    slide2: {
-      backgroundColor: '#B3DC4A',
-    },
-    slide3: {
-      backgroundColor: '#6AC0FF',
-    },
-  };
-
-    <div style={Object.assign({}, styles.slide, styles.slide1)}>slide n°1</div>
-
-  */
 
 
 const DiscoveryGame = () => {
@@ -40,18 +19,25 @@ const DiscoveryGame = () => {
         ref={el => (reactSwipeEl = el)}
       >
         <div>
-          <button onClick={() => reactSwipeEl.next()} className='Swipe-right'>Next</button>
+          <div className="SongName">Jermas Face</div>
+          <div className="SongDuration">22:15:12</div>
+          
+          
           <img src={scope} className='trove__mp_img'/>
+          <button onClick={() => reactSwipeEl.next()} className='Swipe-right'>Next</button>
           <button onClick={() => reactSwipeEl.prev()} className='Swipe-Left'>Previous</button>
+          
         </div>
         <div>
-          <button onClick={() => reactSwipeEl.next()} className='Swipe-right'>Next</button>
+          
           <img src={MrElbertson} className='trove__mp_img'/>
+          <button onClick={() => reactSwipeEl.next()} className='Swipe-right'>Next</button>
           <button onClick={() => reactSwipeEl.prev()} className='Swipe-Left'>Previous</button>
         </div>
         <div>
-          <button onClick={() => reactSwipeEl.next()} className='Swipe-right'>Next</button>
+          
           <img src={Jermy} className='trove__mp_img'/>
+          <button onClick={() => reactSwipeEl.next()} className='Swipe-right'>Next</button>
           <button onClick={() => reactSwipeEl.prev()} className='Swipe-Left'>Previous</button>
         </div>
         
@@ -59,6 +45,7 @@ const DiscoveryGame = () => {
 
         
       </ReactSwipe>
+      
     </div>
   );
 };
