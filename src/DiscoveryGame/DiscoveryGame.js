d01357d43ae01d144e77674f5ea6ab0e49cbad6d
Facelesscactus <113211038+Facelesscactus@users.noreply.github.com>
2023-01-30 00:35:33 -0700

    The jermy update

--- src/DiscoveryGame/DiscoveryGame.js
+++ src/DiscoveryGame/DiscoveryGame.js
@@ -1,3 +1,4 @@
+import ReactAudioPlayer from 'react-audio-player';
 import './DiscoveryGame.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -5,6 +6,8 @@ import ReactSwipe from 'react-swipe';
 import scope from '../imgs/ScaryJerma.png'
 import MrElbertson from '../imgs/jerma-burger.gif'
 import Jermy from '../imgs/jerma.gif'
+import MeepMerp from '../mp3/MeepMerp.mp3'
+import OTheJermy from '../mp3/Jerma_misery.mp3'
  
 
 
@@ -20,19 +23,23 @@ const DiscoveryGame = () => {
       >
         <div>
           <div className="SongName">Jermas Face</div>
-          <div className="SongDuration">22:15:12</div>
-          
-          
-          <img src={scope} className='trove__mp_img'/>
-          <button onClick={() => reactSwipeEl.next()} className='Swipe-right'>Next</button>
-          <button onClick={() => reactSwipeEl.prev()} className='Swipe-Left'>Previous</button>
-          
+          <div className="control">
+            <img src={scope} className='trove__mp_img'/>
+            <button onClick={() => reactSwipeEl.next()} className='Swipe-right'>Next</button>
+            <button onClick={() => reactSwipeEl.prev()} className='Swipe-Left'>Previous</button>
+          </div>
+        
+          <ReactAudioPlayer src={MeepMerp} autoPlay controls className="SongDuration"/>
+        
         </div>
         <div>
-          
+          <div className="SongName">Jermas Face</div>
+          <div className="control">
           <img src={MrElbertson} className='trove__mp_img'/>
           <button onClick={() => reactSwipeEl.next()} className='Swipe-right'>Next</button>
           <button onClick={() => reactSwipeEl.prev()} className='Swipe-Left'>Previous</button>
+          </div>
+          <ReactAudioPlayer src={OTheJermy} autoPlay controls className="SongDuration"/>
         </div>
         <div>
           
