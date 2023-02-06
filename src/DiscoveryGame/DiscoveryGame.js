ebe636b2144c028b6dfd69f17e6f5cece9cb29f0
Facelesscactus <113211038+Facelesscactus@users.noreply.github.com>
2023-02-05 19:58:26 -0700

    No longer loops slides

--- src/DiscoveryGame/DiscoveryGame.js
+++ src/DiscoveryGame/DiscoveryGame.js
@@ -7,11 +7,11 @@ import { useSwipeable } from 'react-swipeable';
  
 
 
-const DiscoveryGame= () =>{
+const DiscoveryGame = () => {
   const [index, setIndex] = React.useState(0);
-  const [accept, setaccept] = React.useState(0);
-  const [deny, setdeny] = React.useState(0);
-  
+  const [accept, setAccept] = React.useState(0);
+  const [deny, setDeny] = React.useState(0);
+
   const slides = [
     {
       url: require('../imgs/Reflection.jpg'),
@@ -31,26 +31,46 @@ const DiscoveryGame= () =>{
       header: 'Farewell',
       audio: require('../mp3/Farewell.mp3'),
     },
+    {
+      url: require('../imgs/Farewell.jpg'),
+      alt: 'Image 3',
+      header: 'got through the Discover Game',
+      audio: require('../mp3/Farewell.mp3'),
+    },
   ];
-
- 
 
   const swipeableProps = useSwipeable({
     trackMouse: true,
-    onSwipedLeft: () => {setIndex(prevIndex => (prevIndex + 1) % slides.length);
-    setaccept(accept + 1)},
-    onSwipedRight: () => {setIndex(prevIndex => (prevIndex + 1) % slides.length);
-    setdeny(deny + 1)},
+    onSwipedLeft: () => {
+      if (index === slides.length - 1) return;
+      setIndex((prevIndex) => (prevIndex + 1) % slides.length);
+      setAccept(accept + 1);
+    },
+    onSwipedRight: () => {
+      if (index === slides.length - 1) return;
+      setIndex((prevIndex) => (prevIndex + 1) % slides.length);
+      setDeny(deny + 1);
+    },
   });
   
   return (
     <div className="div">
       <div className='container' {...swipeableProps}>
-        <h2 className="SongName">{slides[index].header}</h2>
+      <h2 className="SongName">{slides[index].header}</h2>
         <img src={slides[index].url} alt={slides[index].alt} className="trove__mp_img"/>
         <ReactAudioPlayer src={slides[index].audio} className="SongDuration" autoPlay controls/>
-        <button onClick={() => {setIndex(prevIndex => (prevIndex + 1) % slides.length);setdeny(deny + 1)}} className='Swipe-Left'>Previous</button>
-        <button onClick={() => {setIndex(prevIndex => (prevIndex + 1) % slides.length);setaccept(accept + 1)}} className='Swipe-right'>Next</button>
+        
+        
+        <button onClick={() => {if (index === slides.length - 1) return;
+        setIndex((prevIndex) => (prevIndex + 1) % slides.length);
+        setDeny(deny + 1);
+        }} className='Swipe-Left'>Previous</button>
+
+
+        <button onClick={() => {if (index === slides.length - 1) return;
+        setIndex((prevIndex) => (prevIndex + 1) % slides.length);
+        setAccept(accept + 1);
+        }} className='Swipe-right'>Next</button>
         
       </div>
       <div className="show-number">{accept}jeff</div>
